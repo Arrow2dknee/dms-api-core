6dccfbcad6628ef48d067af54e0b9e0cd598e3ea
Arrow2dknee <yozziedummy@gmail.com>
2023-07-18 01:57:20 +0530

    Add controller endpoints for file operations

--- src/files/file.pb.ts
+++ src/files/file.pb.ts
@@ -24,10 +24,68 @@ export interface CreateFileResponse {
   data: File | undefined;
 }
 
+/** Update file path */
+export interface UpdatePathRequest {
+  id: string;
+  folder: string;
+}
+
+export interface UpdatePathResponse {
+  message: string;
+  data: string;
+}
+
+/** Find file by name */
+export interface FindOneRequest {
+  name: string;
+}
+
+export interface FindOneResponse {
+  message: string;
+  data: FileMetadata | undefined;
+}
+
+/** Find all files */
+export interface FindAllRequest {
+  user: string;
+  page: string;
+  limit: string;
+  search: string;
+}
+
+export interface FileMetadata {
+  id: string;
+  name: string;
+  content: string;
+}
+
+export interface FindAllResponse {
+  message: string;
+  data: FileMetadata[];
+}
+
+/** Delete file */
+export interface DeleteFileRequest {
+  id: string;
+}
+
+export interface DeleteFileResponse {
+  message: string;
+  data: string;
+}
+
 export const FILE_PACKAGE_NAME = 'file';
 
 export interface FilesServiceClient {
   createFile(request: CreateFileRequest): Observable<CreateFileResponse>;
+
+  updateFilePath(request: UpdatePathRequest): Observable<UpdatePathResponse>;
+
+  findOne(request: FindOneRequest): Observable<FindOneResponse>;
+
+  findAll(request: FindAllRequest): Observable<FindAllResponse>;
+
+  deleteFile(request: DeleteFileRequest): Observable<DeleteFileResponse>;
 }
 
 export interface FilesServiceController {
@@ -37,11 +95,39 @@ export interface FilesServiceController {
     | Promise<CreateFileResponse>
     | Observable<CreateFileResponse>
     | CreateFileResponse;
+
+  updateFilePath(
+    request: UpdatePathRequest,
+  ):
+    | Promise<UpdatePathResponse>
+    | Observable<UpdatePathResponse>
+    | UpdatePathResponse;
+
+  findOne(
+    request: FindOneRequest,
+  ): Promise<FindOneResponse> | Observable<FindOneResponse> | FindOneResponse;
+
+  findAll(
+    request: FindAllRequest,
+  ): Promise<FindAllResponse> | Observable<FindAllResponse> | FindAllResponse;
+
+  deleteFile(
+    request: DeleteFileRequest,
+  ):
+    | Promise<DeleteFileResponse>
+    | Observable<DeleteFileResponse>
+    | DeleteFileResponse;
 }
 
 export function FilesServiceControllerMethods() {
   return function (constructor: Function) {
-    const grpcMethods: string[] = ['createFile'];
+    const grpcMethods: string[] = [
+      'createFile',
+      'updateFilePath',
+      'findOne',
+      'findAll',
+      'deleteFile',
+    ];
     for (const method of grpcMethods) {
       const descriptor: any = Reflect.getOwnPropertyDescriptor(
         constructor.prototype,
--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Inject, OnModuleInit, Body, Post } from '@nestjs/common';
+import {
+  Controller,
+  Inject,
+  OnModuleInit,
+  Body,
+  Post,
+  Patch,
+  Get,
+  Query,
+  Delete,
+} from '@nestjs/common';
 import { ClientGrpc } from '@nestjs/microservices';
 import { Observable } from 'rxjs';
 
@@ -7,7 +17,15 @@ import {
   FILES_SERVICE_NAME,
   CreateFileRequest,
   CreateFileResponse,
+  UpdatePathRequest,
+  UpdatePathResponse,
+  FindOneRequest,
+  FindOneResponse,
+  FindAllResponse,
+  DeleteFileRequest,
+  DeleteFileResponse,
 } from './file.pb';
+import { PaginationDto } from '../common/dto/pagination.dto';
 
 @Controller('file')
 export class FilesController implements OnModuleInit {
@@ -25,5 +43,36 @@ export class FilesController implements OnModuleInit {
     @Body() bodyDto: CreateFileRequest,
   ): Promise<Observable<CreateFileResponse>> {
     return this.fileService.createFile(bodyDto);
+  }
+
+  @Patch()
+  async updatePath(
+    @Body() bodyDto: UpdatePathRequest,
+  ): Promise<Observable<UpdatePathResponse>> {
+    return this.fileService.updateFilePath(bodyDto);
+  }
+
+  @Get()
+  async findFile(
+    @Query() dto: FindOneRequest,
+  ): Promise<Observable<FindOneResponse>> {
+    return this.fileService.findOne(dto);
+  }
+
+  @Get()
+  async findFiles(
+    @Query() dto: PaginationDto,
+  ): Promise<Observable<FindAllResponse>> {
+    return this.fileService.findAll({
+      ...dto,
+      user: null, // TODO: Update to logged-in user id
+    });
+  }
+
+  @Delete()
+  async remove(
+    @Query() dto: DeleteFileRequest,
+  ): Promise<Observable<DeleteFileResponse>> {
+    return this.fileService.deleteFile(dto);
   }
 }
